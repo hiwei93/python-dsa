f20b88132c6b780a12cce14e89b4aaac37c55aaf
zhangwei <wei1157@hotmail.com>
2022-10-29 21:28:52 +0800

    [打卡] 10.29 数组练习:三数之和,求众数,求缺失的第一个人正数

--- 01_array_practice.py
+++ 01_array_practice.py
@@ -7,6 +7,9 @@
 """
 
 
+from typing import List
+
+
 def merge_ordered_list(l1: list, l2: list) -> list:
     """
     合并两个有序数组, 时间复杂度：O(max(m, n))，空间复杂度o(m + n)
@@ -38,6 +41,172 @@ def merge_ordered_list(l1: list, l2: list) -> list:
     return merged_l
 
 
+class TowSum():
+    """
+    https://leetcode.cn/problems/two-sum/
+    """
+    @staticmethod
+    def solution_1(nums: List[int], target: int) -> List[int]:
+        """
+        时间复杂度: O(n^2)
+        空间复杂度: O(1)
+        """
+        length = len(nums)
+        for i in range(0, length - 1, 1):
+            for j in range(i + 1, length, 1):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+    @staticmethod
+    def solution_2(nums: List[int], target: int) -> List[int]:
+        """
+        时间复杂度: O(n)
+        空间复杂度：O(n)
+        """
+        value2index = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in value2index:
+                return [value2index[diff], i]
+            value2index[num] = i
+        return []
+
+
+class ThreeSum():
+    """
+    https://leetcode.cn/problems/3sum/
+    """
+    @staticmethod
+    def solution(nums: List[int]) -> List[List[int]]:
+        """
+        排序、去重、双指针
+        - 时间复杂度 O(n^2)
+            - 排序O(nlongn) https://www.cnblogs.com/clement-jiao/p/9243066.html
+            - 两层遍历O(n^2)
+        - 空间复杂度O(n) // 主要用在排序上面了
+        """
+        length = len(nums)
+        results = []
+        if length < 3:
+            return results
+
+        nums.sort()
+        for i in range(length):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:  # 优化一：从第二个元素开始去重
+                continue
+            lp, rp = i + 1, length - 1  # 优化二：左右指针，减少一层循环
+            while lp < rp:
+                sum_ = nums[i] + nums[lp] + nums[rp]
+                if sum_ == 0:
+                    results.append([nums[i], nums[lp], nums[rp]])
+                    while lp < rp and nums[lp] == nums[lp + 1]:  # 优化一：去重
+                        lp += 1
+                    while lp < rp and nums[rp] == nums[rp - 1]:  # 优化一：去重
+                        rp -= 1
+                    lp += 1
+                    rp -= 1
+                elif sum < 0:
+                    lp += 1
+                elif sum > 0:
+                    rp -= 1
+        return results
+
+
+class MajorityElement():
+    """
+    https://leetcode.cn/problems/majority-element/
+    """
+    @staticmethod
+    def solution_by_hash(nums: List[int]) -> int:
+        """
+        时间复杂度：O(n)
+        空间复杂度：O(n)
+        """
+        frequent = len(nums) / 2
+        counter = {}
+        for num in nums:
+            if num not in counter:
+                counter[num] = 0
+            counter[num] += 1
+
+        for num, count in counter.items():
+            if count > frequent:
+                return num
+        return None
+
+    @staticmethod
+    def solution_by_sort(nums: List[int]) -> int:
+        """
+        时间复杂度：O(nlogn)
+        空间复杂度：O(1)
+        """
+        nums.sort()
+        return nums[len(nums) // 2]
+
+    @staticmethod
+    def solution_by_boyer_Moore(nums: List[int]) -> int:
+        """
+        时间复杂度：O(n)
+        空间发复杂度：O(1)
+        """
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
+
+
+class FirstMissingPositive():
+    """
+    https://leetcode.cn/problems/first-missing-positive
+    """
+    @staticmethod
+    def solution_by_hash(nums: List[int]) -> int:
+        """
+        模拟哈希表，更改原始数组，增加标记位
+        - 时间复杂度O(n)
+        - 空间复杂度O(1)
+        """
+        length = len(nums)
+        for i in range(length):
+            if nums[i] <= 0:
+                nums[i] = length + 1
+
+        for i in range(length):
+            num = abs(nums[i])
+            if num <= length:
+                nums[num - 1] = -1 * abs(nums[num - 1])
+
+        for i in range(length):
+            if nums[i] > 0:
+                return i + 1
+        return length + 1
+
+    @staticmethod
+    def solution_by_exchange(nums: List[int]) -> int:
+        """
+        时间复杂度：O(n)
+        空间复杂度：O(1)
+        注意：注意替换顺序
+        """
+        length = len(nums)
+        for i in range(length):
+            while nums[i] > 0 and nums[i] <= length and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        for i in range(length):
+            if i + 1 != nums[i]:
+                return i + 1
+        return length + 1
+
+
 def test_merge_ordered_list():
     l1 = [1, 2, 3]
     l2 = [2, 2, 4, 5]
@@ -56,4 +225,4 @@ def test_merge_ordered_list():
 
 
 if __name__ == '__main__':
-    test_merge_ordered_list()
+    FirstMissingPositive.solution_by_exchange([3, 4, -1, 1])
