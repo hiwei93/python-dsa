8b390588c8f4b39f55f17bd97a0bdc9f707e1f1b
zhangwei <wei1157@hotmail.com>
2022-11-16 23:26:05 +0800

    [打卡] 11.16 快速排序实现

--- 06_sort/advance_sort.py
+++ 06_sort/advance_sort.py
@@ -7,7 +7,7 @@ class MergeSort:
 
     分析：
     - 时间复杂度：O(nlogn)
-    - 空间复杂度：O(n)  # TODO: 知道计算方式
+    - 空间复杂度：O(n)  # TODO: 需要知道计算方式
     - 稳定排序
     - 非原地排序算法
     """
@@ -46,3 +46,52 @@ class MergeSort:
             start += 1
         for i, elem in enumerate(tmp):
             nums[p+i] = elem
+
+
+class QuickSort:
+    """
+    递推公式：
+    - f(p, r) = f(p, q - 1) + f(q + 1, r)
+    - 递归基：p >= r
+
+    实现思想：
+    1. 每次选择一个元素就位，并使其左侧元素全部小于该元素，其右侧元素全部大于等于该元素
+    2. 基于该元素，分为左右两个子问题，分别进行排序
+    3. 只有一个元素或者没有元素的时候停止继续
+
+    分析：
+    - 时间复杂度：最优 O(nlogn), 最差 O(n^2)
+    - 空间复杂度： # TODO: 原地排序算法，但是是递归实现方式，递归栈的空间使用如何计算？
+    - 非稳定排序
+    - 原地排序算法
+    """
+    @classmethod
+    def quick_sort(cls, nums: list):
+        n = len(nums)
+        cls.quick_sort_c(nums, 0, n-1)
+
+    @classmethod
+    def quick_sort_c(cls, nums: list, p: int, r: int):
+        if p >= r:
+            return
+        q = cls.partition(nums, p, r)
+        print(f"{p=} {r=} {q=} {nums=}")
+        cls.quick_sort_c(nums, p, q-1)
+        cls.quick_sort_c(nums, q+1, r)
+
+    @classmethod
+    def partition(cls, nums: list, p: int, r: int):
+        """
+        主要思想：
+        1. 通过数据交换实现原地算法
+        2. 通过双指针，将小于povit的元素置换到i指针前面
+        """
+        pivot = nums[r]
+        i = p
+        for j in range(p, r):
+            if nums[j] < pivot:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+        nums[i], nums[r] = nums[r], nums[i]
+        return i
+
--- 06_sort/test_advance_sort.py
+++ 06_sort/test_advance_sort.py
@@ -4,7 +4,7 @@ import sys
 
 sys.path.extend('advance_sort')
 
-from advance_sort import MergeSort
+from advance_sort import MergeSort, QuickSort
 
 
 class TestMergeSort(unittest.TestCase):
@@ -25,5 +25,23 @@ class TestMergeSort(unittest.TestCase):
         self.assertEqual(nums, expected)
 
 
+class TestQuickSort(unittest.TestCase):
+    def test_sort(self):
+        expected = [1, 2, 3, 4, 5, 6]
+        nums = [1, 2, 3, 4, 5, 6]
+        QuickSort.quick_sort(nums)
+        self.assertEqual(nums, expected)
+        nums = [6, 5, 4, 3, 2, 1]
+        QuickSort.quick_sort(nums)
+        self.assertEqual(nums, expected)
+        nums = [3, 1, 4, 2, 6, 5]
+        QuickSort.quick_sort(nums)
+        self.assertEqual(nums, expected)
+        expected = [1, 2, 2, 3, 4, 5]
+        nums = [1, 2, 3, 4, 5, 2]
+        QuickSort.quick_sort(nums)
+        self.assertEqual(nums, expected)
+
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
