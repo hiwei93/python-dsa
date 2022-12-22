0c874c592c03455a7b49fd429ebd5aa3677d2fd9
zhangwei <wei1157@hotmail.com>
2022-12-23 00:45:54 +0800

    [打卡] 12.22 二分查找递归实现

--- 07_binary_search/binary_search.py
+++ 07_binary_search/binary_search.py
@@ -1,12 +1,44 @@
-def binary_search(nums, value: int) -> int:
-    n = len(nums)
-    low, high = 0, n - 1
-    while low <= high:
+class BinarySearchIteration(object):
+    """
+    二分查找，迭代实现
+    - 时间复杂度：O(logn)
+    - 空间复杂度：O(1)
+    """
+    @classmethod
+    def search(cls, nums, value: int) -> int:
+        n = len(nums)
+        low, high = 0, n - 1
+        while low <= high:
+            mid = low + ((high - low) >> 1)
+            if nums[mid] == value:
+                return mid
+            if nums[mid] < value:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+
+
+class BinarySearchRecursive(object):
+    """
+    二分查找，递归实现
+    - 时间复杂度：O(logn)
+    - 空间复杂度：O(logn)
+    """
+    @classmethod
+    def search(cls, nums, value: int) -> int:
+        n = len(nums)
+        low, high = 0, n - 1
+        return cls.bsearch(nums, low, high, value)
+
+    @classmethod
+    def bsearch(cls, nums, low, high, value):
+        if low > high:
+            return -1
+
         mid = low + ((high - low) >> 1)
         if nums[mid] == value:
             return mid
         if nums[mid] < value:
-            low = mid + 1
-        else:
-            high = mid - 1
-    return -1
+            return cls.bsearch(nums, mid + 1, high, value)
+        return cls.bsearch(nums, low, mid - 1, value)
--- 07_binary_search/test_binary_search.py
+++ 07_binary_search/test_binary_search.py
@@ -4,14 +4,19 @@ import sys
 
 sys.path.extend("binary_search")
 
-from binary_search import binary_search
+from binary_search import BinarySearchIteration, BinarySearchRecursive
 
 
 class TestBinarySearch(unittest.TestCase):
-    def test_binary_search(self):
+    def test_binary_search_iteration(self):
         nums, v = [8, 11, 19, 23, 27, 33, 45, 55, 67, 98], 23
         expected = 3
-        self.assertEqual(binary_search(nums, v), expected)
+        self.assertEqual(BinarySearchIteration.search(nums, v), expected)
+
+    def test_binary_search_recursive(self):
+        nums, v = [8, 11, 19, 23, 27, 33, 45, 55, 67, 98], 23
+        expected = 3
+        self.assertEqual(BinarySearchRecursive.search(nums, v), expected)
 
 
 if __name__ == '__main__':
